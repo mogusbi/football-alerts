a29fc02840c37d725c8c5444ba374f04207cbff7
Mo Gusbi <mo@Mos-MacBook-Pro.local>
2019-04-16 23:18:44 +0100

    User login hook lambda

---
+++ packages/api/src/users/handler.ts
@@ -0,0 +1,51 @@
+import {CognitoUserPoolEvent, Handler} from 'aws-lambda';
+import {DynamoDB} from 'aws-sdk';
+
+const documentClient: DynamoDB.DocumentClient = new DynamoDB.DocumentClient();
+const TableName: string = process.env.TABLE_NAME;
+
+function conditionalValue (input: string): string {
+  if (input === undefined) {
+    return null;
+  }
+
+  return input;
+}
+
+export const register: Handler = async (event: CognitoUserPoolEvent): Promise<CognitoUserPoolEvent> => {
+  switch (event.triggerSource) {
+  case 'PostAuthentication_Authentication':
+  case 'PostConfirmation_ConfirmSignUp':
+    const lastUpdate: Date = new Date();
+
+    await documentClient
+      .update({
+        ExpressionAttributeValues: {
+          ':emailAddress': event.request.userAttributes.email,
+          ':forename': event.request.userAttributes.given_name,
+          ':lastUpdate': lastUpdate.toUTCString(),
+          ':surname': event.request.userAttributes.family_name,
+          ':twitterHandle': conditionalValue(event.request.userAttributes.twitter_handle),
+          ':website': conditionalValue(event.request.userAttributes.website)
+        },
+        Key: {
+          id: event.request.userAttributes.sub
+        },
+        TableName,
+        UpdateExpression: `
+          set emailAddress = :emailAddress,
+          forename = :forename,
+          lastUpdate = :lastUpdate,
+          surname = :surname,
+          twitterHandle = :twitterHandle,
+          website = :website
+        `
+      })
+      .promise();
+
+    break;
+  default:
+  }
+
+  return event;
+};
