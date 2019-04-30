c012d0e008c18df436e3be6d39d7d210d828957c
Mo Gusbi <mo@Mos-MacBook-Pro.local>
2019-04-30 23:40:39 +0100

    Update config for appsync

--- applications/admin/src/pages/Clubs/ClubsAdd.jsx
+++ applications/admin/src/pages/Clubs/ClubsAdd.jsx
@@ -1,8 +1,26 @@
+// import {API, graphqlOperation} from 'aws-amplify';
 import React from 'react';
 import ClubsForm from './ClubsForm';
 
+// const mutation = `mutation CreateClub($input: ClubInput!) {
+//   createClub(input: $input) {
+//     id
+//     name
+//   }
+// }`;
+
 function ClubsAdd () {
-  function submit () {}
+  async function submit (input) {
+    // try {
+    //   const club = await API.graphql(graphqlOperation(mutation, {
+    //     input
+    //   }));
+    //
+    //   console.log(club);
+    // } catch (e) {
+    //   console.log(e);
+    // }
+  }
 
   return (
     <ClubsForm onSubmit={submit} />
