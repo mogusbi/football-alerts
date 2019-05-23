dbbc92241277d540cc1595ce26c48140f19f1962
Mo Gusbi <mo@Mos-MacBook-Pro.local>
2019-05-23 23:32:15 +0100

    Update image upload path

--- applications/admin/src/pages/Images/index.jsx
+++ applications/admin/src/pages/Images/index.jsx
@@ -1,3 +1,4 @@
+import {S3Image} from 'aws-amplify-react';
 import PropTypes from 'prop-types';
 import React, {Fragment, memo, useEffect} from 'react';
 import {connect} from 'react-redux';
@@ -16,6 +17,8 @@ function Index ({getImagesHandler, nextImagesHandler}) {
   return (
     <Fragment>
       <PageTitle title='Images' />
+
+      <S3Image imgKey='media/img/311756ea-1271-40e5-b845-5549940bff7c/main.jpg' />
 
       <button type='button' onClick={() => nextImagesHandler('')}>Load more</button>
     </Fragment>
